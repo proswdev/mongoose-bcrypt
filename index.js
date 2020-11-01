0c94405ab9b19e8c37f4ee0c3e42b6440db97760
Philipp Lypniakov <filipprayden@gmail.com>
2020-11-01 19:54:42 +0200

    feat(index): Add support for Model.insertMany

--- index.js
+++ index.js
@@ -116,6 +116,57 @@ module.exports = function(schema, options) {
     }
   });
 
+  // Hash all modified encrypted fields upon inserting many documents
+  schema.pre('insertMany', function (next, documents) {
+    documents.forEach(function(document) {
+      var changed = [];
+
+      // Determine list of encrypted fields that have been modified
+      fields.forEach(function(field){
+        const splitField = field.split('.');
+        // we are concerned only about the leftmost part
+        if (document.hasOwnProperty(splitField[0])) {
+          changed.push(splitField);
+        }
+      });
+
+      // Create/update hash for each modified encrypted field
+      var count = changed.length;
+      if (count > 0) {
+        changed.forEach(function(splitField){
+          var value;
+          var nestedObjWithValue;
+          var lastField;
+          var originalField = splitField.join('.');
+
+          splitField.forEach(function(field, idx, thisArr) {
+            const lastIdx = thisArr.length - 1;
+            value = value ? value[field] : document[field];
+            lastField = field;
+            if(idx < lastIdx) nestedObjWithValue = value;
+          });
+
+          nestedObjWithValue = nestedObjWithValue ? nestedObjWithValue : document;
+
+          if (typeof value === 'string') {
+            encrypt(originalField, value, function(err, hash) {
+              if (err) return next(err);
+              nestedObjWithValue[lastField] = hash;
+              if (--count === 0)
+                next();
+            });
+          } else {
+            nestedObjWithValue[lastField] = '';
+            if (--count === 0)
+              next();
+          }
+        });
+      } else {
+        next();
+      }
+    });
+  });
+
   function getUpdateField(update, name) {
     if (update.hasOwnProperty(name)) {
       return update[name];
